1d3314abb521f20b93a6203f914e7aae88422087
Daniel-Davis <ddv9461@saintjoe.edu>
2015-10-14 18:53:01 -0700

    Update Title_Screen.java

--- Title_Screen.java
+++ Title_Screen.java
@@ -1,64 +1,42 @@
 package Game;
 
 import java.awt.*;
+
 import javax.swing.*;
+
 import java.util.Timer;
 
-
-public class Title_Screen extends JFrame{
-	
-	//variables//
-	private Screen s;
-	private Image bg;
-	private boolean loaded;
-	private Button play;
-	private Button exit;
-	Timer timer = new Timer();
-	
-	
-	//methods//
-	// main //
-	public static void main(String[] args) {
-		DisplayMode dm = new DisplayMode(1900,1200,16,DisplayMode.REFRESH_RATE_UNKNOWN); // creates local displaymode equal to the size of bg image
-		Title_Screen ts = new Title_Screen(); // creates local title screen
-		ts.run(dm); // runs the screen
-	} // end of main
-	
-	// load pictures //
-	public void loadPics() {
-		bg = new ImageIcon("C:\\Users\\Daniel\\workspace\\Pong_Game\\src\\resources\\Pics\\.Title_Screen.jpg").getImage(); // sets global variable bg to get the back ground image
-		loaded = true; // images are loaded
-		repaint(); // repaints/refreshes the screen
-	} // end of loadPics method
-	
-	
-	// run //
-	public void run(DisplayMode dm) {
+public class Title_Screen {
+	//main//
+	public static void main(String args[]){
+		//setting up the screen//
 		
-		setBackground(Color.blue); // test background
-		setForeground(Color.yellow); // text front color
-		setFont(new Font("Ariel",Font.PLAIN, 24)); // test font
-		loaded = false; // no images are loaded
-		s = new Screen(); // creates a new instance of screen class
-		play = new Button(); // creates new play button instance
-		exit = new Button(); // creates new exit button instance
+		JFrame title_frame = new JFrame("Legend of Pong"); // creates a frame to use
+		// Background image //
 		
-		try{
-			s.setFullScreen(dm, this); // sets it to full screen
-			loadPics(); // loads images for screen
-			try{
-				Thread.sleep(5000); // displays my test view for 5 seconds
-			}catch(Exception ex){}
-		}finally{
-			s.restoreScreen(); // then exits the screen view
-		}
-	} // end of run method
-	
-	// paint //
-	public void paint(Graphics g) {
-		if(loaded == true) { // if images are loaded
-			g.drawImage(bg,0,0,null); // draws background image - - - - FIX!! NO IMAGE DISPLAYS?????
-		}// end of if
-	}// end of paint method
+		JLabel Background = new JLabel(new ImageIcon("C:\\Users\\Daniel\\workspace\\Pong_Game\\src\\resources\\Pics\\titleScreen.jpg")); // gets and holds the background image
+		title_frame.setContentPane(Background); // sets the background image
+		// Buttons //
+		
+		JButton Play = new JButton(); // creates the play button
+		Play.setLocation(100, 400); // sets button location
+		Play.setPreferredSize(new Dimension(50,75)); // sets button size
+		Play.setVisible(true); // displays button
+		Play.setEnabled(true); // makes it an active button
+		JButton Exit = new JButton();
+		Exit.setLocation(200,400); // sets button location
+		Exit.setPreferredSize(new Dimension(50,75)); // sets button size
+		Exit.setVisible(true); // displays button
+		Exit.setEnabled(true); // makes it an active button
+		// Title of Game //
+		
+		JLabel Title = new JLabel("Legend of Pong");
+		title_frame.pack();
+		title_frame.setResizable(false);
+		title_frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		title_frame.setVisible(true);
+		
+	}// end of main
 	
 }// end of class
+
