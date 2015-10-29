268a9e5817537fe85f984686a1d71d60d6ac5365
Daniel-Davis <ddv9461@saintjoe.edu>
2015-10-28 18:19:42 -0700

    Update Game_Screen.java

--- Game_Screen.java
+++ Game_Screen.java
@@ -9,8 +9,12 @@ import java.util.Timer;
 
 public class Game_Screen {
 	
+	//Methods//
+	
+	
 	//main//
 	public static void main(String args[]){
+	/////Building the display/////
 		JFrame game_frame = new JFrame("Legend of Pong");
 		JLabel Background = new JLabel(new ImageIcon("C:\\Users\\Daniel\\workspace\\Pong_Game\\src\\resources\\Pics\\gameScreen.jpg"));
 		game_frame.setContentPane(Background);
@@ -18,6 +22,13 @@ public class Game_Screen {
 		game_frame.setResizable(false);
 		game_frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		game_frame.setVisible(true);
+		
+		//setting the header section//
+		JLabel Score = new JLabel();
+		Timer Timer = new Timer();
+		JLabel Life = new JLabel();
+		
+		
 		
 	}// end of main
 	
