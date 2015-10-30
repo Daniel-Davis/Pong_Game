7b75a8a9796551a336f8b238840f0e3acf79723e
Daniel-Davis <ddv9461@saintjoe.edu>
2015-10-29 21:13:21 -0700

    Update Game_Screen.java

--- Game_Screen.java
+++ Game_Screen.java
@@ -2,17 +2,18 @@ package Game;
 
 //Imports//
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-import java.util.Timer;
-
 public class Game_Screen {
+	//Variables//
+	private static int Score = 0;
+	private static int Life = 3;
+	Timer timer;
 	
-	//Methods//
-	
-	
-	//main//
+	//Main//
 	public static void main(String args[]){
 	/////Building the display/////
 		JFrame game_frame = new JFrame("Legend of Pong");
@@ -24,13 +25,52 @@ public class Game_Screen {
 		game_frame.setVisible(true);
 		
 		//setting the header section//
-		JLabel Score = new JLabel();
-		Timer Timer = new Timer();
-		JLabel Life = new JLabel();
 		
-		
+		//Score//
+		int score = getScore(); // int to hold score value
+		JLabel scoreL = new JLabel(); // label to display score
+		scoreL.setText(String.valueOf(score)); // connects label and int
+		scoreL.setBounds(100, 15, 200, 35); // sets location of score
+		scoreL.setFont(new Font("Courier New", Font.BOLD, 20)); // sets font and size of font
+		scoreL.setForeground(Color.GREEN); // sets color of Score
+		game_frame.add(scoreL); // adds score
+		//Timer// //???//
+		//Life//
+		int life = getLife(); // int to hold value of lives left
+		JLabel lifeL = new JLabel(); // label to hold life
+		lifeL.setText(String.valueOf(life)); // connects label and int
+		lifeL.setBounds(700, 15, 200, 35); // sets location of life
+		lifeL.setFont(new Font("Courier New", Font.BOLD, 20)); // sets font and size of font
+		lifeL.setForeground(Color.GREEN); // sets color of life
+		game_frame.add(lifeL); // adds life
 		
 	}// end of main
 	
+	//Setters and getters//
+	public static int getScore() {
+		int score=Score;
+		return score;
+	}// end of getScore
+	public static int setScore(int score) {
+		score=Score;
+		return score;
+	}// end of setScore
+	public static int getLife() {
+		int life=Life;
+		return life;
+	}// end of getLife
+	public static int setLife(int life) {
+		life=Life;
+		return life;
+	}// end of setLife
+	//End Of Setters & Getters//
+	
+	
+	//Methods//
+	public void update(){
+		
+		
+		
+	} // end of update
 	
 }// end of class
