c6d7cba14331e2710b05c0f6cd31a2934df93720
Daniel-Davis <ddv9461@saintjoe.edu>
2015-11-02 17:34:25 -0800

    Create Ball.java

---
+++ Ball.java
@@ -0,0 +1,22 @@
+package Game;
+
+import javax.swing.ImageIcon;
+
+public class Ball {
+	private int xdir,ydir;
+	
+	//constructor//
+	public Ball() {
+		xdir=1;
+		ydir=-1;
+		
+		ImageIcon png = new ImageIcon("C:\\Users\\Daniel\\workspace\\Pong_Game\\src\\resources\\Pics\\.Ball.png");
+		image = png.getImage();
+		
+	} // end of constructor
+	
+	
+	
+	
+	
+}// end of Ball class
