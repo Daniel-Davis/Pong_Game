f7c9b0b5edde32cec75cc4a9086959e7bedc3d13
Daniel-Davis <ddv9461@saintjoe.edu>
2015-11-12 17:47:32 -0800

    Update Game_Screen.java

--- Game_Screen.java
+++ Game_Screen.java
@@ -2,69 +2,72 @@ package Game;
 
 //Imports//
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
+import java.util.Timer;
+
 public class Game_Screen {
+	
 	//Global Variables//
 	int ScreenHeight = 500;
 	int ScreenWidth = 800;
-	static boolean running = true;
+	boolean running = true;
 	static long Score = 0;
 	static int Life = 3;
 	static int Time;
+	Timer timer;
+	
+	//Constructor//
+	public Game_Screen() {
+	JFrame game_frame = new JFrame("Legend of Pong");
+	JLabel Background = new JLabel(new ImageIcon("C:\\Users\\Daniel\\workspace\\Pong_Game\\src\\resources\\Pics\\gameScreen.jpg"));
+	game_frame.setContentPane(Background);
+	game_frame.pack();
+	game_frame.setResizable(false);
+	game_frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+	game_frame.setVisible(true);
+	
+	//setting the header section//
+	
+	//Score//
+	long score = getScore(); // int to hold score value
+	JLabel scoreL = new JLabel(); // label to display score
+	scoreL.setText(String.valueOf(score)); // connects label and int
+	scoreL.setBounds(100, 15, 200, 35); // sets location of score
+	scoreL.setFont(new Font("Courier New", Font.BOLD, 20)); // sets font and size of font
+	scoreL.setForeground(Color.GREEN); // sets color of Score
+	game_frame.add(scoreL); // adds score
+	
+	//Timer//
+	int time = getTime();
+	JLabel timeL = new JLabel(); // label to hold time
+	timeL.setText(String.valueOf(time)); // connects label and int
+	timeL.setBounds(400, 15, 200,35); // set location of time
+	timeL.setFont(new Font("Courier New", Font.BOLD, 20)); // sets font and size of font
+	timeL.setForeground(Color.GREEN); // sets color
+	game_frame.add(timeL); // adds time
+	
+	//Life//
+	int life = getLife(); // int to hold value of lives left
+	JLabel lifeL = new JLabel(); // label to hold life
+	lifeL.setText(String.valueOf(life)); // connects label and int
+	lifeL.setBounds(700, 15, 200, 35); // sets location of life
+	lifeL.setFont(new Font("Courier New", Font.BOLD, 20)); // sets font and size of font
+	lifeL.setForeground(Color.GREEN); // sets color of life
+	game_frame.add(lifeL); // adds life	
+	
+	//Paddle//?
+
+	
+	}// end of screen constructor
+	
 	
 	
 	//Main//
 	public static void main(String args[]){
-	/////Building the display/////
-		JFrame game_frame = new JFrame("Legend of Pong");
-		JLabel Background = new JLabel(new ImageIcon("C:\\Users\\Daniel\\workspace\\Pong_Game\\src\\resources\\Pics\\gameScreen.jpg"));
-		game_frame.setContentPane(Background);
-		game_frame.pack();
-		game_frame.setResizable(false);
-		game_frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		game_frame.setVisible(true);
-		
-		//setting the header section//
-		
-		//Score//
-		long score = getScore(); // int to hold score value
-		JLabel scoreL = new JLabel(); // label to display score
-		scoreL.setText(String.valueOf(score)); // connects label and int
-		scoreL.setBounds(100, 15, 200, 35); // sets location of score
-		scoreL.setFont(new Font("Courier New", Font.BOLD, 20)); // sets font and size of font
-		scoreL.setForeground(Color.GREEN); // sets color of Score
-		game_frame.add(scoreL); // adds score
-		
-		//Timer// //WORK ON NOW!!!!//
-		int time = getTime();
-		JLabel timeL = new JLabel(); // label to hold time
-		timeL.setText(String.valueOf(time)); // connects label and int
-		timeL.setBounds(400, 15, 200,35); // set location of time
-		timeL.setFont(new Font("Courier New", Font.BOLD, 20)); // sets font and size of font
-		timeL.setForeground(Color.GREEN); // sets color
-		game_frame.add(timeL); // adds time
-		
-		//Life//
-		int life = getLife(); // int to hold value of lives left
-		JLabel lifeL = new JLabel(); // label to hold life
-		lifeL.setText(String.valueOf(life)); // connects label and int
-		lifeL.setBounds(700, 15, 200, 35); // sets location of life
-		lifeL.setFont(new Font("Courier New", Font.BOLD, 20)); // sets font and size of font
-		lifeL.setForeground(Color.GREEN); // sets color of life
-		game_frame.add(lifeL); // adds life
-		//Paddle//
-		Paddle link = new Paddle();
-		//Finish above code!!!!//
-		
-		//run()
-
-		
-		
-		
+		Game_Screen gs = new Game_Screen();
+		gs.start();
 	}// end of main
 	
 	///Methods///
@@ -91,46 +94,34 @@ public class Game_Screen {
 	// end of getters and setters
 	
 	//game loop//
-	public static void run(){
-		//variables//
-		final int TARGET_FPS = 60;
-		final int OPTIMAL_TIME = 1000000000 / TARGET_FPS;
-		long lastLoopTime = System.nanoTime();
-		long lastFpsTime = 0;
-		
-		while(running == true) {
-			//local variables//
-			long start = System.nanoTime();
-			lastLoopTime = start;
-			long elapsed = start - lastLoopTime;
-	        double delta = elapsed / ((double)OPTIMAL_TIME);
-			
-	        lastFpsTime += elapsed;
-	        if(lastFpsTime >= 1000000000) {
-	            lastFpsTime = 0;
-	        } // end of if
-			
-	        update(delta);
-	        
-			redraw();
-	        
-			
-			try {
-				Thread.sleep( (lastLoopTime-System.nanoTime() + OPTIMAL_TIME)/1000000 );
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			} // end of try and catch
-		
-		} // end of while loop
-			
-			
-		
-	} // end of run method
-
+	public void start(){
+		timer.schedule(new gameLoop(), 0, 1000/60);
+	} // end of loop method
 	
-	//game update//
-	private static void update(double delta) {
+	private class gameLoop extends java.util.TimerTask{
+		public void run() {
+			if(!running) { // running is false
+				this.cancel(); // stop timer/loop
+			} // end of if
+			else if(Life == 0){
+				JOptionPane.showMessageDialog(null, "Game Over");
+				this.cancel();
+			}// end of else if
+			else {
+			update();
+			redraw();
+			} // end of else
+			
+			
+			
+		}// end of run
+	} // end of gameLoop
+	
+	//updates anything needing an update//
+	private static void update() {
 		Time++;
+		
+		
 		
 		
 	} // end of update
